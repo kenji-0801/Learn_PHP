66789636d04234d874e329d92b9c72afeb6c330d
kenji-0801 <70939128+kenji-0801@users.noreply.github.com>
2020-11-18 22:29:27 +0900

    :tada: learn about comments

---
+++ 01_comments.php
@@ -0,0 +1,18 @@
+<?php
+// Single line comment
+// 一行のみのコメントアウトができる
+
+# Another single line comment
+# 一行のみのコメントアウトができる
+
+/*
+Multiline comment with some long texts
+Multiline comment with some long texts
+Multiline comment with some long texts
+*/
+
+/*
+複数行のコメントアウトができる
+複数行のコメントアウトができる
+複数行のコメントアウトができる
+*/
